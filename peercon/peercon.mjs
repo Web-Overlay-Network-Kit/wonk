b15c0f00eb40ee903041358b755c6b1b96d36ffe
Evan Brass <brassevan@gmail.com>
2023-06-01 08:45:12 -0700

    start over
    I should really just add things as I build them instead of laying it all out initially.

--- peercon/peercon.mjs
+++
@@ -1,7 +0,0 @@
-// It's ok to use top-level await here, because this library can only be used in pages (since we need RTCPeerConnection which doesn't exist inside workers)
-import './identity.mjs';
-
-
-export default class PeerCon extends RTCPeerConnection {
-	
-}
