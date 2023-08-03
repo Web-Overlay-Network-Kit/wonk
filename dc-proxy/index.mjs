7efc80928c8438805406179e47ab12e50fab7adb
Evan Brass <brassevan@gmail.com>
2023-08-03 07:52:00 -0700

    _

--- dc-proxy/index.mjs
+++ dc-proxy/index.mjs
@@ -12,21 +12,30 @@ export class DcProxy extends EventTarget {
 	async get_readyState() {}
 	async get_bufferedAmount() {}
 	get bufferedAmountLowThreshold() { return this.#bufferedThreshold; }
-	set bufferedAmountLowThreshold(_) {}
+	set bufferedAmountLowThreshold(value) {
+		this.#bufferedThreshold = value;
+		this.#inner.postMessage({ bufferedAmountLowThreshold: value });
+		return true;
+	}
 
 	constructor(inner, options = {}) {
 		this.#inner = inner;
 		Object.assign(this, options);
+
+		this.#inner.addEventListener('message', ({data}) => {
+
+		});
 	}
 
-	async close() {}
-	send() {}
+	close() {}
+	send(data) {}
 }
 
 export function proxy_dc(dc) {
 	const { port1: theirs, port2: ours } = new MessageChannel();
 
 	ours.onmessage = async function handler() {};
+
 	dc.addEventListener('open', () => {});
 	dc.addEventListener('message', () => {});
 	dc.addEventListener('error', () => {});
